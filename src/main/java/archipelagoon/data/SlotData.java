3751fff1c0ad46b69cc5ffb73d97bdad9b0a0436
Patrick Kolb <64711466+pkolb-dev@users.noreply.github.com>
2026-02-21 20:20:57 -0600

    Bump versions
    set new version to 0.1.5
    set expected apworld to 0.1.1

--- src/main/java/archipelagoon/data/SlotData.java
+++ src/main/java/archipelagoon/data/SlotData.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Set;
 
 public final class SlotData {
-  public static final Set<String> EXPECTED_MOD_VERSIONS = new HashSet<>(List.of("0.1.0"));
+  public static final Set<String> EXPECTED_MOD_VERSIONS = new HashSet<>(List.of("0.1.1"));
 
   @SerializedName("addition_randomizer")
   public int additionRandomizer = 1;
